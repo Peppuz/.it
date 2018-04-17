b47a39573baaaa60541ced22ad37b97b4571ebde
Peppuz <ip3c0r1n4@gmail.com>
2018-04-18 00:48:02 +0200

    1:

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -6,3 +7,14 @@ app = Flask(__name__)
 def index():
 	return "Pz"
 
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        f = request.files['the_file']
+        f.save('/var/www/uploads/' + secure_filename(f.filename))
+
+
+@app.route('/tracks')
+def login():
+    return redirect(url_for('https://soundcloud.com/peppuz/tracks'))
