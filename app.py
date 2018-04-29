cf0aceee6a7da6f80ca80d582bd7ffb08a9cecf9
Peppuz <ip3c0r1n4@gmail.com>
2018-04-29 15:31:38 +0200

    Testing commits FDD Backend

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, abort, redirect, url_for, request, render_template
+from flask import Flask, abort, redirect, url_for, request, render_template, jsonify
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
@@ -29,3 +29,36 @@ def facebook():
 @app.route("/2stoned")
 def youtube2():
 	return redirect('https://ddg.gg')
+
+@app.route('/fdd' method='POST')
+def fondoDaniloDolci():
+	"""
+		* takes the POST data
+		* adds new buttons to attivita
+		* generates new Files to upload with fdd_upload(page)
+
+	"""
+	buttons = []
+	title = ''
+	year  = ''
+	# TODO: add auto set Year from currentDate
+	return  jsonify(request.POST)
+
+	if request.method == 'POST':
+		for key, val in request.form:
+			if key == 'title' and not val:
+				return 'Errore: non è stato inserito alcun Titolo! \nTorna indietro e ricompila il modulo correttamente.'
+			else:
+				title = val
+			if key == 'year' and not val:
+				year = val
+
+			# TODO: Check encoding for URI UTF-8
+			# TODO: ADD button to array as Dictionary with title,url
+			if key[0:7] == 'buttons-':
+				buttons.append(val)
+
+
+
+
+	# return render_template('index.html')
