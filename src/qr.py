13c567f56d431b66bc70d58dee2ef31b97efad5b
Peppuz <ip3c0r1n4@gmail.com>
2018-05-28 18:45:27 +0200

    QR Now supports slashes

--- src/qr.py
+++ src/qr.py
@@ -1,26 +1,27 @@
 from src import app
-from flask import redirect, render_template, request, send_from_directory
-import qrcode
+from flask import redirect, render_template, request, send_from_directory, jsonify
+import qrcode, urllib.parse
 
 # QR Generator
 @app.route('/qr', methods=['POST', 'GET'])
 def qrgen():
 	if request.method == 'POST' and request.form:
 		data = request.form['qr']
-		if "/" in data:
-			return render_template('qr.html', error="Slashes '/' are not allowed")
 
-		qr = qr = qrcode.QRCode(
+		qr = qrcode.QRCode(
 		    version=1,
 		    error_correction=qrcode.constants.ERROR_CORRECT_L,
 		    box_size=10,
 		    border=4)
 		qr.add_data(data)
 		qr.make(fit=True)
-		img = qr.make_image(fill_color="black", back_color="transparent")
-		img.save(open("src/static/qr/%s.png"%data,'wb'))
 
-		img = "qr/%s.png" % data
+		filename = urllib.parse.quote_plus(data)
+
+		img = qr.make_image(fill_color="black", back_color="transparent")
+		img.save(open("src/static/qr/%s.png" % filename,'wb'))
+		img = "qr/%s.png" % filename
+
 		return render_template('qr.html', data=data, img=img)
 	return render_template('qr.html')
 
@@ -28,10 +29,7 @@ def qrgen():
 def qr(data=None):
 	if not data:
 		render_template('qr.html')
-	if "/" in data:
-		return render_template('qr.html', error="Error: Slashes '/' are not allowed")
-
-
+		
 	qr = qr = qrcode.QRCode(
 	    version=1,
 	    error_correction=qrcode.constants.ERROR_CORRECT_L,
