4486a2691ab921ab6d05ea53749ac086a7325706
Peppuz <ip3c0r1n4@gmail.com>
2018-06-02 14:42:54 +0200

    Deprecated Inline QR

--- src/qr.py
+++ src/qr.py
@@ -25,20 +25,3 @@ def qrgen():
 		return render_template('qr.html', data=data, img=img)
 	return render_template('qr.html')
 
-@app.route('/qr/<data>')
-def qr(data=None):
-	if not data:
-		render_template('qr.html')
-		
-	qr = qr = qrcode.QRCode(
-	    version=1,
-	    error_correction=qrcode.constants.ERROR_CORRECT_L,
-	    box_size=10,
-	    border=4)
-	qr.add_data(data)
-	qr.make(fit=True)
-	img = qr.make_image(fill_color="black", back_color="transparent")
-	img.save(open("src/static/qr/%s.png"%data,'wb'))
-
-	img = "qr/%s.png" % data
-	return send_from_directory('static', img)
